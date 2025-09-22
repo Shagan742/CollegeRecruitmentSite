5f288f490cbb5332804a750eb54625a14fe08c33
Shagan742 <526ssangha@frhsd.com>
2025-09-22 15:00:14 -0400

    finished all content; started javascript stuff

---
+++ script.js
@@ -0,0 +1,9 @@
+// declare variables
+
+let SATScore = document.getElementById("SATScore");
+let ACTScore = document.getElementById("ACTScore");
+let rankType= document.getElementById("rankType");
+let classRank=document.getElementById("classRank");
+let GPAValue=document.getElementById("GPAValue");
+let GPAType=document.getElementById("GPAType");
+let SubmitBtn=document.getElementById("SubmitBtn");
